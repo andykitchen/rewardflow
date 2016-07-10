2f7e813e6e176fe5d9a47eaa3740bd89e9616659
Andy Kitchen <kitchen.andy+git@gmail.com>
2016-07-11 00:36:57 +1000

    better threading

--- vis.py
+++ vis.py
@@ -23,86 +23,84 @@ def np_rgb_to_qimage(x):
 	qimage = QtGui.QImage(x.data, x.shape[1], x.shape[0], QtGui.QImage.Format_RGB888)
 	return qimage
 
-#frame = np.zeros((ale_screen_height, ale_screen_width, ale_channels))
-#np.random.randn(ale_screen_height, ale_screen_width, ale_channels)
-
 
 class AleGraphicsItem(QtGui.QGraphicsItem):
 	def __init__(self):
 		super(AleGraphicsItem, self).__init__()
-		qimage = np_rgb_to_qimage(np.zeros((ale_screen_height, ale_screen_width, ale_channels)))
-		self.frame_image = qimage
+		self.frame_image = None
 
 	def boundingRect(self):
 		return QtCore.QRectF(0, 0, ale_screen_width, ale_screen_height)
 
 	def paint(self, painter, option, widget):
-		painter.drawImage(0, 0, self.frame_image)
-	
+		if self.frame_image is None:
+			painter.fillRect(0, 0, ale_screen_width, ale_screen_height, QtCore.Qt.gray)
+		else:
+			painter.drawImage(0, 0, self.frame_image)
+
+	@QtCore.pyqtSlot()
 	def show_frame(self, frame_image):
 		self.update()
 		self.frame_image = frame_image
-	
 
-class AleUpdateSignalHolder(QtCore.QObject):
+
+class AleCompute(QtCore.QObject):
 	frame = QtCore.pyqtSignal(QtGui.QImage)
 
-class AleThread(threading.Thread):
-	def __init__(self, coord, rom_path):
-		super(AleThread, self).__init__()
-		self.coord = coord
+	def __init__(self, rom_path):
+		super(AleCompute, self).__init__()
 		self.ale = ale_python_interface.ALEInterface()
 		self.ale.loadROM(rom_path)
 		self.actions = self.ale.getMinimalActionSet()
-		self.signals = AleUpdateSignalHolder()
+		self.timer = QtCore.QTimer()
+		self.timer.setSingleShot(True)
+		self.timer.timeout.connect(self.step)
 
+	@QtCore.pyqtSlot()
 	def run(self):
-		with coord.stop_on_exception():
-			while not self.coord.should_stop():
-				time.sleep(1./60)
-				self.step()
-	
+		self.timer = QtCore.QTimer()
+		self.timer.setSingleShot(True)
+		self.timer.timeout.connect(self.step)
+		self.step()
+
+	@QtCore.pyqtSlot()
 	def step(self):
+		start = time.clock()
+		self.step_ale()
+		finish = time.clock()
+		elapsed = finish - start
+		target = 1./60
+		delay = target - elapsed if elapsed < target else 0
+		self.timer.start(1000 * delay)
+
+	def step_ale(self):
 		self.ale.act(random.choice(self.actions))
 		frame = self.ale.getScreenRGB()
-		qimage = np_rgb_to_qimage(frame)
-		self.signals.frame.emit(qimage)
+		qimage = np_rgb_to_qimage(frame).copy()
+		self.frame.emit(qimage)
 
 
-def setup_ale_thread(coord):
-	thread = AleThread(coord, rom_path)
+def setup_ale_thread():
+	thread = QtCore.QThread()
+	compute = AleCompute(rom_path)
 	ale_graphics_item = AleGraphicsItem()
-	thread.signals.frame.connect(ale_graphics_item.show_frame, type=QtCore.Qt.QueuedConnection)
 
-	return thread, ale_graphics_item
+	compute.moveToThread(thread)
+	compute.frame.connect(ale_graphics_item.show_frame)
+	thread.started.connect(compute.run)
 
-
-def setup_view(ale_graphics_item):
-	scene = QtGui.QGraphicsScene()
-	view = QtGui.QGraphicsView(scene)
-
-	scene.addItem(ale_graphics_item)
-
-	# timer = QtCore.QTimer()
-	# timer.timeout.connect(ale_graphics_item.step)
-
-	return view, scene, ale_graphics_item
+	return thread, ale_graphics_item, compute
 
 
 if __name__ == '__main__':
 	app = QtGui.QApplication(sys.argv)
+	scene = QtGui.QGraphicsScene()
+	view = QtGui.QGraphicsView(scene)
 
-	coord = tf.train.Coordinator()
-
-	thread, ale_graphics_item = setup_ale_thread(coord)
-	view, scene, ale_graphics_item = setup_view(ale_graphics_item)
-
-	thread.start()
-	# timer.start()
+	thread, ale_graphics_item, compute = setup_ale_thread()
+	scene.addItem(ale_graphics_item)
 
 	view.show()
+	thread.start()
 
-	code = app.exec_()
-	coord.request_stop()
-	coord.join([thread])	
-	sys.exit(code)
+	sys.exit(app.exec_())
